46e72bbde89470574935f8065bfee87937fe4721
Taankydaanky <amet.kemal@googlemail.com>
2018-12-08 18:12:33 +0100

    some ui fixes

--- Frontend/JS/functions.js
+++ Frontend/JS/functions.js
@@ -1,20 +1,5 @@
 var e = document.getElementById("meinRechteckCanvas");
 var canv = e.getContext("2d");
 canv.fillStyle = "black";
-canv.fillRect(75,0,400,400);
-console.log(e);
+canv.fillRect(0,0,250,400);
 
-function save(){
-// Funktion zum speichern
-console.log("SAVE");
-}
-function reset(){
-// Funktion zum resetten
-confirm("Alles wird zurückgesetzt!");
-console.log("RESET");
-}
-function back(){
-// FUnktion zum zurücksetzen
-confirm("Das letzte Objekt wird gelöscht!");
-console.log("BACK");
-}
